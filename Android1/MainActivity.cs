13ef260a828e4fb491efb192660bea22e39816bd
UxDeveloper82 <electronicsmf@aol.com>
2020-03-25 18:14:14 -0400

    First commit

---
+++ Android1/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Android1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        SeekBar seekBar;
+        TextView resultTextView;
+        Button rollButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
+            //resultTextView.Text = "9";
+            rollButton = (Button)FindViewById(Resource.Id.rollbutton);
+            rollButton.Click += RollButton_Click;
+            
+        }
+
+        private void RollButton_Click(object sender, System.EventArgs e)
+        {
+            Random random = new Random();
+            int luckyNumber = random.Next(seekBar.Progress) + 1;
+            resultTextView.Text = luckyNumber.ToString();
+        }
+    }
+}
+
